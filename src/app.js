fe026990a7e5e6b483cb54b90bb84740c1aa711b
Prakash Kumar <prakash.kumar@ftxinfotech.com>
2022-12-02 13:36:25 +0530

    code

--- src/app.js
+++ src/app.js
@@ -3,11 +3,21 @@ import MemberList from "./components/MemberList";
 import SearchBar from "./components/searchBar";
 import data from "../data.json";
 import "./app.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Main = () => {
   const [memberData, setmemberData] = useState(data);
+  useEffect(() => {
+    apiCall();
+  });
 
+  async function apiCall() {
+    const res = await fetch("https://api.github.com/users/prakashrajotiya");
+    console.log("res", res);
+    const data = await res.json();
+    console.log("data", data);
+    console.log(data);
+  }
   return (
     <div className="main">
       <div className="searchbar">
